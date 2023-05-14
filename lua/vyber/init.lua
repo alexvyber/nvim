b706a6df66796c3eb966bbbc162e6cfb6dd98b81
Alexey Sokolov <alexvyber@gmail.com>
2023-05-14 15:30:36 +0300

    rmeove

--- lua/vyber/init.lua
+++
@@ -1,9 +0,0 @@
-require "vyber.packer"
-require "vyber.remap"
-require "vyber.set"
-require "vyber.options"
-require "vyber.ab"
-
--- colorscheme
--- require "vyber.rose-pine"
-require "vyber.catppuccin"
